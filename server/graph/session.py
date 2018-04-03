409aa8f0a741f52700589aa8189f8123587f94d8
Ian Brennan <ian.brennan@viget.com>
2018-04-02 19:15:01 -0500

    removed seed, added validation to session creation and added exists query to make it more performant

--- server/graph/session.py
+++ server/graph/session.py
@@ -1,11 +1,16 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from models.teacher import Teacher as TeacherModel
+from models.subject import Subject as SubjectModel
 from models.session import Session as SessionModel
+from models.department import Department as DepartmentModel
 from models.foundation import db_session
 from department import Department
-from teacher import Teacher
 from subject import Subject
+from teacher import Teacher
+
+from sqlalchemy import exists
 
 
 class Session(SQLAlchemyObjectType):
@@ -30,13 +35,30 @@ class CreateSession(graphene.Mutation):
         room = graphene.Int()
 
     def mutate(self, info, name, department, subject, teacher, room):
-        dept = Department.get_query(info).filter_by(id=department).one()
-        teacher = Teacher.get_query(info).filter_by(id=teacher).one()
-        subject = Subject.get_query(info).filter_by(id=subject).one()
-        new_session = SessionModel(
-            name=name, department=dept, subject=subject, teacher=teacher, room=room)
-        db_session.add(new_session)
-        db_session.commit()
+        dept_exists = db_session.query(exists().where(
+            DepartmentModel.id == department)).scalar()
+        teacher_exists = db_session.query(
+            exists().where(TeacherModel.id == teacher)).scalar()
+        subject_exists = db_session.query(
+            exists().where(SubjectModel.id == subject)).scalar()
+        if dept_exists and teacher_exists and subject_exists:
+            new_session = SessionModel(
+                name=name, department_id=department, subject_id=subject, teacher_id=teacher, room=room)
+            db_session.add(new_session)
+            db_session.commit()
+        else:
+            exception_string = 'Error! '
+            if not dept_exists:
+                exception_string = exception_string + \
+                    'department with id {} was not found '.format(department)
+            if not teacher_exists:
+                exception_string = exception_string + \
+                    'teacher with id {} was not found '.format(teacher)
+            if not subject_exists:
+                exception_string = exception_string + \
+                    'subject with id {} was not found '.format(subject)
+
+            raise Exception(exception_string)
 
         return CreateSession(
             id=new_session.id,
